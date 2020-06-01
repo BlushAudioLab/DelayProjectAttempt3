2033a2789ff876f57e70142a3a60501d1cfbcd8c
Blush Audio Dev <blushaudio@gmail.com>
2020-06-01 21:16:08 +0100

    Initial Commit
    Trying Again (For Fuck's Sake)

---
+++ Source/PluginEditor.h
@@ -0,0 +1,35 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class DelayProjectAttempt3AudioProcessorEditor  : public AudioProcessorEditor
+{
+public:
+    DelayProjectAttempt3AudioProcessorEditor (DelayProjectAttempt3AudioProcessor&);
+    ~DelayProjectAttempt3AudioProcessorEditor();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    DelayProjectAttempt3AudioProcessor& processor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DelayProjectAttempt3AudioProcessorEditor)
+};
